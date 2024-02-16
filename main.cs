e1ac2f40efe37b8d9f94985cfee99e2f9a492d1d
Andregynouz <158660413+Andregynouz@users.noreply.github.com>
2024-02-16 13:12:24 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,46 @@
+using System;
+
+class Program
+{
+    public static void Main (string[] args)
+{
+    double value = 150;
+
+    switch(value) {
+        case 0.25:
+            Console.WriteLine("No person is found in Php. 0.25", value);
+            break;
+        case 0.5:
+            Console.WriteLine("No person is found in Php. 0.5");
+            break;
+        case 0.1:
+            Console.WriteLine("No person is found in Php. 0.1");
+            break;
+        case 1:
+            Console.WriteLine("Jose Rizal is found in Php. 1");
+            break;
+        case 5:
+            Console.WriteLine("Emillio Aguinaldo is found in Php. 5");
+            break;
+        case 10:
+            Console.WriteLine("Andres Bonifacio & Apolinario Mabini is found in Php. 10");
+            break;
+        case 50:
+            Console.WriteLine("Sergio Osmena is found in Php. 50");
+            break;
+        case 100:
+            Console.WriteLine("Manuel Roxas is found in Php. 100");
+            break;
+        case 200:
+            Console.WriteLine("Diosdado Macapagal is found in Php. 200");
+            break;
+        case 500:
+            Console.WriteLine("Benigno Sr. and Corazon Aquino is found in Php. 500");
+            break;
+        case 1000:
+            Console.WriteLine("Jose Abad Santos, Vicente Lim, Josefa Llanes Escoda is found in Php. 1000");
+            break;
+
+}
+}
+}
